36124f13257ce437cc1d77875a2b6829355dc0db
Victor Adalto <victor.adalto@institutoeducar.com.br>
2021-07-25 23:24:31 -0300

    Commit iniciado salvar dados de cliente

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  FaAngleDoubleRight,
+  FaUserFriends,
+  FaShoppingCart,
+} from "react-icons/fa";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  let nomeCliente: string = '';
+  let cep: string = '';
+  async function carregarCEP():Promise<any> {
+    return await axios.get(`https://viacep.com.br/ws/${cep}/json/`).then((retorno:any) => { console.log(retorno)});
+  }
+  function cadastrarCliente(){
+    const regexCEP = /^[0-9]{8}$/;
+    if(regexCEP.test(cep)){
+      let Clientes = localStorage.getItem("clientes");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar">
+        <ul className="navbar-nav">
+          <li className="logo">
+            <a href="/#" className="nav-link">
+              <FaAngleDoubleRight />
+              <span className="logo-text">Menu</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/#" className="nav-link">
+              <FaUserFriends></FaUserFriends>
+              <span className="link-text">Clientes</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/#" className="nav-link">
+              <FaShoppingCart></FaShoppingCart>
+              <span className="link-text">Produtos</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <input onChange={(e) => (nomeCliente = e.target.value)} type="text" placeholder="Nome do cliente"></input>
+        <input onChange={(e) => (nomeCliente = e.target.value)} type="text" placeholder="CEP do cliente"></input>
+        <button type="button"
+						onClick={cadastrarCliente}>Cadastrar!</button>
+      </main>
+    </>
   );
 }
 
---
+++ src/interfaces.ts
@@ -0,0 +1,11 @@
+export interface Cliente {
+    Nome:string,
+    CEP:string,
+    Logradouro:string,
+    UF:string
+}
+
+export interface Produto {
+    Nome: string,
+    Valor: number,
+}
